6956d7a81858db9d9b42d06cf244ea094da5aa28
Avery <avery.chen23@hotmail.com>
2023-09-07 07:24:26 +0800

    feat: 讀入的輸入值轉換成數字型別再比較

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, cmp::Ordering};
 use rand::Rng;
 
 fn main() {
@@ -15,5 +15,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("讀取該行輸入失敗");
 
-    println!("你猜的數字：{guess}")
+
+    let guess:u32 = guess.trim().parse().expect("請輸入你猜的數字：");
+
+    println!("你猜的數字：{guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => print!("太小了！"),
+        Ordering::Greater=> print!("太大了！"),
+        Ordering::Equal => print!("猜中了！")
+    }
 }
