732bd690329bd11f1a23085033a0e772f938cc9e
Avery <avery.chen23@hotmail.com>
2023-09-07 08:27:39 +0800

    feat: 加入迴圈允許多次猜測數字

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,28 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("祕密數字為：{secret_number}");
 
-    println!("請輸入你猜的數字：");
+    loop {
+        println!("請輸入你猜的數字：");
 
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("讀取該行輸入失敗");
-
-
-    let guess:u32 = guess.trim().parse().expect("請輸入你猜的數字：");
-
-    println!("你猜的數字：{guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => print!("太小了！"),
-        Ordering::Greater=> print!("太大了！"),
-        Ordering::Equal => print!("猜中了！")
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("讀取該行輸入失敗");
+    
+    
+        let guess:u32 = guess.trim().parse().expect("請輸入你猜的數字：");
+    
+        println!("你猜的數字：{guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => print!("太小了！"),
+            Ordering::Greater=> print!("太大了！"),
+            Ordering::Equal => {
+                print!("猜中了！");
+                break;
+            }
+        }        
     }
+
 }
