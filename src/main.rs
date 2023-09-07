d4c191e80266d2c9c449f651bbcc5dbca6643f26
Avery <avery.chen23@hotmail.com>
2023-09-07 08:40:20 +0800

    feat: 顯示可猜測的數字範圍

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use std::{io, cmp::Ordering};
 use rand::Rng;
 
 fn main() {
-    println!("請猜一個數字！");
-
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("祕密數字為：{secret_number}");
-
+    let mut min = 1;
+    let mut max = 100;
+    let secret_number = rand::thread_rng().gen_range(min..=max);
     loop {
+        println!("請從{min}到{max}猜一個數字！");
+
         println!("請輸入你猜的數字：");
 
         let mut guess = String::new();
@@ -25,8 +25,14 @@ fn main() {
         println!("你猜的數字：{guess}");
     
         match guess.cmp(&secret_number) {
-            Ordering::Less => print!("太小了！"),
-            Ordering::Greater=> print!("太大了！"),
+            Ordering::Less => {
+                print!("太小了！");
+                min = guess + 1;
+            },
+            Ordering::Greater=> {
+                print!("太大了！");
+                max = guess - 1;
+            },
             Ordering::Equal => {
                 print!("猜中了！");
                 break;
